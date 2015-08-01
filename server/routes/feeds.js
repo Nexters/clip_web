da57495deab1c667da239044853a38e6a9f33477
Donghyun Go <godong9@gmail.com>
2015-08-01 16:07:38 +0900

    Update routes

--- server/routes/feeds.js
+++ server/routes/feeds.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var log4js = require('log4js');
-var logger = log4js.getLogger('routes/feeds');
 var feedCtrl = require('../controllers/Feed');
 
 /* GET feeds */
