6272b3440764a60abe9d84fd72b5eb0252959cb7
jiye0n <jeony12@gmail.com>
2015-08-05 20:29:27 +0900

    font,image,server

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,24 @@ router.get('/', function(req, res) {
     res.render('main.ejs', data);
 });
 
+router.get('/signin', function(req, res, next) {
+    res.render('signin', { title: 'My World', name: 'Ji Yeon' });
+});
+
+router.get('/signup', function(req, res, next) {
+    res.render('signup', { title: 'My World', name: 'Ji Yeon' });
+});
+
+
+router.post('/test', function(req, res, netx) {
+    console.log(req.body);
+});
+
+router.post('/test2', function(req, res, netx) {
+    console.log(req.body);
+});
+
+
 router.get('/:id', userCtrl.getUserPage);
 
 module.exports = router;
