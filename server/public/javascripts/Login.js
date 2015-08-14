73ec359f3459e167d66c1b7f419fa032eba2aa58
garin0 <garin0112@naver.com>
2015-08-14 19:17:23 +0900

    signin js 수

--- server/public/javascripts/Login.js
+++ server/public/javascripts/Login.js
@@ -1,12 +1,23 @@
 (function login() {
-    function init() {
+
+
         $('#signin_btn').click(function() {
             console.log($('#input_email').val());
-            alert("sing in");
-        });
-    }
 
-    $(document).ready(function() {
-        init();
+
+            if($('.id').val()==""){
+                alert("이메일 주소를 입력하세요.");
+                $('.id').focus();
+                return false;
+            }else if($('.pw').val()==""){
+                alert("비밀번호를 입력하세요.");
+                $('.pw').focus();
+                return false;
+            }else{
+                alert("로그인 했습니다.");
+            }
+
     });
+
 }());
+
