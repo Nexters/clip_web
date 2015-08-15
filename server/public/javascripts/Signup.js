456b253626e869e7cc3bee87bd5a3ea3ed68d129
jiye0n <jeony12@gmail.com>
2015-08-15 15:44:26 +0900

    Update signup.js

--- server/public/javascripts/Signup.js
+++ server/public/javascripts/Signup.js
@@ -1,15 +1,63 @@
-/**
- * Created by jiyeon on 15. 8. 14..
- */
+(function login() {
+    function init() {
+        $("#signup_btn").click(function(){
+            var minLength = 6;
+            var $textarea = $('#input_password1');
+            var hasError = false;
+            var nameval = $("#input_name").val();
+            var emailval = $("#input_email1").val();
+            var passwordVal = $("#input_password1").val();
+            var checkVal = $("#input_conpass").val();
+
+            $(".error").hide();
+
+            if(nameval ==''){
+                alert("Name을 입력하세요");
+                $("#input_name").focus();
+
+            }
+            if($textarea.val().length < minLength) {
+                alert('비밀번호를 6자 이상으로 입력해 주세요.');
+                return false;
+            }
+            if (passwordVal != checkVal ) {
+                $("#input_conpass").after(function() {
+                    alert('비밀번호가 다릅니다.');
+                    $("#input_conpass").focus();
+                });
+                hasError = true;
+            }
+
+            else if(emailval ==''){
+                alert('Email을 입력하세요');
+                $("#input_email1").focus();
 
 
+            }
 
+            else if (passwordVal == '') {
+                $("#input_password1").after(function() {
+                    alert('Passwords를 입력하세요');
+                    $("#input_conpass").focus();
 
+                });
+                hasError = true;
+            }
+            else if (checkVal == '') {
+                $("#input_conpass").after(function() {
+                    alert('비밀번호를 입력해주세요');
+                    $("#input_conpass").focus();
+                });
+                hasError = true;
+            }
 
-jQuery(function(){
+            if(hasError == true) {return false;}
+        });
+    }
 
-    $(document).ready(function()
-    {
+    $(document).ready(function() {
+        init();
+
         $("#input_name").keyup(function(event){
             if (!(event.keyCode >=37 && event.keyCode<=40)) {
                 var inputVal = $(this).val();
@@ -17,67 +65,4 @@ jQuery(function(){
             }
         });
     });
-
-    $("#signup_btn").click(function(){
-        var minLength = 6;
-        var $textarea = $('#input_password1');
-        var hasError = false;
-        var nameval = $("#input_name").val();
-        var emailval = $("#input_email1").val();
-        var passwordVal = $("#input_password1").val();
-        var checkVal = $("#input_conpass").val();
-
-        $(".error").hide();
-
-        if(nameval ==''){
-            alert("Name을 입력하세요");
-            $("#input_name").focus();
-
-        }
-        if($textarea.val().length < minLength) {
-            alert('비밀번호를 6자 이상으로 입력해 주세요.');
-            return false;
-        }
-        if (passwordVal != checkVal ) {
-            $("#input_conpass").after(function() {
-                alert('비밀번호가 다릅니다.');
-                $("#input_conpass").focus();
-            });
-            hasError = true;
-        }
-
-        else if(emailval ==''){
-            alert('Email을 입력하세요');
-            $("#input_email1").focus();
-
-
-        }
-
-        else if (passwordVal == '') {
-            $("#input_password1").after(function() {
-                alert('Passwords를 입력하세요');
-                $("#input_conpass").focus();
-
-            });
-            hasError = true;
-        }
-        else if (checkVal == '') {
-            $("#input_conpass").after(function() {
-            alert('비밀번호를 입력해주세요');
-            $("#input_conpass").focus();
-        });
-            hasError = true;
-        }
-
-        if(hasError == true) {return false;}
-    });
-});
-
-
-
-    //$(document).ready(function(){
-    //    $("#signup_btn").click(function(){
-    //        $("#input_email1").hide("fast");
-    //    });
-    //});
-
+}());
