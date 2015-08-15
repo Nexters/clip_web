ec4423bebcefb373d35d35a81f8fc43ed6ec56d6
whdms705 <whdms705@nate.com>
2015-08-15 20:18:19 +0900

    clip 정보 테스트

--- server/controllers/Clip.js
+++ server/controllers/Clip.js
@@ -12,7 +12,43 @@ function ClipCtrl() {
 
 ClipCtrl.getUserClips = function(req, res) {
     // TODO: 여기에 유저 클립 목록 가져오는 코드 작성해야함
+    var criteria,errors;
+    req.checkParams('id', 'Invalid id').notEmpty();
+    errors = req.validationErrors();
+    if (errors) return res.status(400).send(Result.ERROR(errors));
+    criteria ={_id: req.params.id}
+    Clip.getClips(criteria,function(err,doc){
+        res.status(200).send(Result.SUCCESS(doc));
+    });
 
 };
+
+UserCtrl.saveUserClips = function(req, res) {
+    var errors, clipData;
+    var title,feeds,keywords;
+
+    req.checkBody('title', 'Invalid title').notEmpty();
+    req.checkBody('feeds', 'Invalid feeds').notEmpty();
+    req.checkBody('keywords', 'Invalid keywords').notEmpty();
+    errors = req.validationErrors();
+    if (errors) return res.status(400).send(Result.ERROR(errors));
+    clipData = {
+        title:req.body.title,
+        feeds:req.body.feeds,
+        keywords:req.body.keywords
+    };
+    Clip.getClips({$or:[{title: clipData.title}, {feeds: clipData.feeds}]}, function(err, user) {
+        if (err) res.status(400).send(Result.ERROR(err));
+        if (user) res.status(400).send(Result.ERROR("이미 존재하는 clip정보"));
+        Clip.saveClip(clipData, function(err, doc) {
+            return res.status(200).send(Result.SUCCESS(doc));
+        });
+    });
+
+
+
+
+}
+
 
 module.exports = ClipCtrl;
