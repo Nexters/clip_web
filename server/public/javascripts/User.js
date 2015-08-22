b9fc03b728e9b84214be77cd210d0c66e703ca65
Donghyun Go <godong9@gmail.com>
2015-08-22 10:41:25 +0900

    Update user page modal

--- server/public/javascripts/User.js
+++ server/public/javascripts/User.js
@@ -2,6 +2,11 @@
     'use strict';
 
     function init() {
+        new Taggle('keyword-box', {
+            placeholder: '태그 입력',
+            duplicateTagClass: 'bounce'
+        });
+
         $('#keyword_setting_btn').click(function() {
             $('#setting_modal').modal('show');
             initModal();
@@ -9,13 +14,7 @@
     }
 
     function initModal() {
-        new Taggle('keyword-box', {
-            placeholder: '태그 입력',
-            duplicateTagClass: 'bounce'
-        });
-
-        $('#modal_add_btn').click(function() {
-            console.log("post");
+        $('#modal_add_btn').unbind('click').click(function() {
             HttpUtil.postData('/feed/check', {feed: $('#modal_input_site').val()}, function(err, result) {
                 if (err) return alert('등록할 수 없는 사이트입니다.(RSS를 지원하는 사이트만 추가 가능합니다.)');
                 alert("추가되었습니다.");
