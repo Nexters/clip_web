f7c9c250f64c53d376c21b6abcb1ee99c1e36383
Donghyun Go <godong9@gmail.com>
2015-08-22 09:55:27 +0900

    Updaet

--- server/public/javascripts/Home.js
+++ server/public/javascripts/Home.js
@@ -198,7 +198,7 @@
                     '<img src="/images/clip_btn.png" align="center">'+
                 '</span>'+
                 '<div class="img-box">'+
-                    '<img class="title-img" src="'+feed.image+'" align="middle" onError="this.src=\'+"/images/card_no_image.png"+\'">'+
+                    '<img class="title-img" src="'+feed.image+'" align="middle" onError="this.src='+"\'/images/card_no_image.png\'"+'">'+
                 '</div>'+
                 '<div class="title-box">'+
                     '<p class="title-txt">'+feed.title+'</p>'+
@@ -224,7 +224,7 @@
             '<li data-id="'+feed.id+'">'+
                 '<div class="img-wrapper">'+
                     '<img class="card-delete-btn" src="/images/card_delete_icon.png">'+
-                    '<img class="card-image" src="'+feed.src+'" onError="this.src=\'+"/images/card_no_image.png"+\'">'+
+                    '<img class="card-image" src="'+feed.src+'" onError="this.src='+"\'/images/card_no_image.png\'"+'">'+
                     '<span class="title-wrapper">'+
                         '<p>'+feed.title+'</p>'+
                     '</span>'+
