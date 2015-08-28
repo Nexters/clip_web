ddbb520f519a22a467db08204a73a68626f91e8a
jiye0n <jeony12@gmail.com>
2015-08-28 19:47:32 +0900

    Update
    modal scroll, 한글제한

--- server/public/javascripts/User.js
+++ server/public/javascripts/User.js
@@ -36,13 +36,6 @@
             duplicateTagClass: 'bounce'
         });
 
-
-        <!-- modal 버튼 부분 -->
-        $('#keyword_setting_btn').click(function() {
-            $('#setting_modal').modal('show');
-            initModal();
-        });
-
         <!-- 보드 타이틀 부분 -->
         $(".board").click(function(){
             $(".myclip_title").text($(this).data('title'));
@@ -94,6 +87,11 @@
             }
         });
 
+        <!-- modal 버튼 부분 -->
+        $('#keyword_setting_btn').click(function() {
+            $('#setting_modal').modal('show');
+            initModal();
+        });
 
         $('.sidebar-setting-icon').click(function() {
             $('#profile_modal').modal('show');
@@ -108,6 +106,40 @@
         }
         $('.site_list > li').append(button);
     }
+
+    function initModal() {
+        $.extend(true, newUserData, userData);
+
+        initModalSiteItems();
+
+        $('#modal_add_btn').unbind('click').click(function() {
+            HttpUtil.postData('/feed/check', {feed: $('#modal_input_site').val()}, function(err, result) {
+                if (err) return alert('등록할 수 없는 사이트입니다.(RSS를 지원하는 사이트만 추가 가능합니다.)');
+                alert("추가되었습니다.");
+                newUserData.feeds.push(result);
+                initModalSiteItems();
+                $('#modal_input_site').val('');
+            });
+        });
+
+
+        $('.site-delete-btn').unbind('click').click(function() {
+            var feedIndex = newUserData.feeds.indexOf($(this).parent().text());
+            newUserData.feeds.splice(feedIndex, 1);
+            $(this).parent().remove();
+            console.log(newUserData.feeds);
+        });
+
+        $('#modal_key_btn').unbind('click').click(function() {
+            newUserData.keywords = taggle.getTagValues();
+            HttpUtil.putData('/user/id/'+newUserData._id, {feeds: newUserData.feeds, keywords: newUserData.keywords}, function(err, result) {
+                if (err) return alert(err);
+                alert("등록되었습니다.");
+                location.reload(true);
+            });
+        });
+    }
+
 
     //프로필 설정 모달
     function initUserSettingModal() {
@@ -134,6 +166,18 @@
 
         <!-- 프로필 이름 설정 부분 -->
         $('#profile_name_input').val(userData.name);
+        $(document).ready(function() {
+            init();
+
+            $("#profile_name_input").keyup(function(event){
+                if (!(event.keyCode >=37 && event.keyCode<=40)) {
+                    var inputVal = $(this).val();
+                    $(this).val(inputVal.replace(/[^a-z0-9]/gi,''));
+                     alert("한글은 입력할수 없습니다");
+                }
+            });
+        });
+
         $('#change_pw_input').val(userData.pw);
         $('#change_pw_confirm').val(userData.pw);
         <!-- 저장하기 버튼 -->
@@ -197,38 +241,6 @@
 
     }
 
-    function initModal() {
-        $.extend(true, newUserData, userData);
-
-        initModalSiteItems();
-
-        $('#modal_add_btn').unbind('click').click(function() {
-            HttpUtil.postData('/feed/check', {feed: $('#modal_input_site').val()}, function(err, result) {
-                if (err) return alert('등록할 수 없는 사이트입니다.(RSS를 지원하는 사이트만 추가 가능합니다.)');
-                alert("추가되었습니다.");
-                newUserData.feeds.push(result);
-                initModalSiteItems();
-                $('#modal_input_site').val('');
-            });
-        });
-
-
-        $('.site-delete-btn').unbind('click').click(function() {
-            var feedIndex = newUserData.feeds.indexOf($(this).parent().text());
-            newUserData.feeds.splice(feedIndex, 1);
-            $(this).parent().remove();
-            console.log(newUserData.feeds);
-        });
-
-        $('#modal_key_btn').unbind('click').click(function() {
-            newUserData.keywords = taggle.getTagValues();
-            HttpUtil.putData('/user/id/'+newUserData._id, {feeds: newUserData.feeds, keywords: newUserData.keywords}, function(err, result) {
-                if (err) return alert(err);
-                alert("등록되었습니다.");
-                location.reload(true);
-            });
-        });
-    }
 
 
 
