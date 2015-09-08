241ff93f55a5ccaba5a67c3819d3d260845bae88
Donghyun Go <godong9@gmail.com>
2015-09-08 22:24:26 +0900

    Update HttpUtil

--- server/public/javascripts/util/HttpUtil.js
+++ server/public/javascripts/util/HttpUtil.js
@@ -1,81 +1,83 @@
-function HttpUtil() {
+var HttpUtil = {};
 
-}
+(function ()  {
 
-HttpUtil.getData = function(url, params, callback) {
-    $.ajax({
-        url: url,
-        type: 'GET',
-        data: params,
-        error: function errorHandler(jqXHR, textStatus, errorThrown) {
-            callback("Error");
-        },
-        success: function successHandler(data, status, xhr) {
-            if (data.resultCode === 0) {
-                callback(null, data.data)
-            } else {
+    HttpUtil.getData = function(url, params, callback) {
+        $.ajax({
+            url: url,
+            type: 'GET',
+            data: params,
+            error: function errorHandler(jqXHR, textStatus, errorThrown) {
                 callback("Error");
+            },
+            success: function successHandler(data, status, xhr) {
+                if (data.resultCode === 0) {
+                    callback(null, data.data)
+                } else {
+                    callback("Error");
+                }
             }
-        }
-    });
-};
+        });
+    };
 
-HttpUtil.postData = function(url, params, callback) {
-    $.ajax({
-        url: url,
-        type: 'POST',
-        contentType: 'application/json;charset=UTF-8',
-        dataType: 'json',
-        data: JSON.stringify(params),
-        error: function errorHandler(jqXHR, textStatus, errorThrown) {
-            callback("Error", null);
-        },
-        success: function successHandler(data, status, xhr) {
-            if (data.resultCode === 0) {
-                callback(null, data.data);
-            } else {
-                callback("Error", data.errors);
-            }
-        }
-    });
-};
-
-HttpUtil.putData = function(url, params, callback) {
-    $.ajax({
-        url: url,
-        type: 'PUT',
-        contentType: 'application/json;charset=UTF-8',
-        dataType: 'json',
-        data: JSON.stringify(params),
-        error: function errorHandler(jqXHR, textStatus, errorThrown) {
-            callback("Error", null);
-        },
-        success: function successHandler(data, status, xhr) {
-            if (data.resultCode === 0) {
-                callback(null, data.data)
-            } else {
+    HttpUtil.postData = function(url, params, callback) {
+        $.ajax({
+            url: url,
+            type: 'POST',
+            contentType: 'application/json;charset=UTF-8',
+            dataType: 'json',
+            data: JSON.stringify(params),
+            error: function errorHandler(jqXHR, textStatus, errorThrown) {
                 callback("Error", null);
+            },
+            success: function successHandler(data, status, xhr) {
+                if (data.resultCode === 0) {
+                    callback(null, data.data);
+                } else {
+                    callback("Error", data.errors);
+                }
             }
-        }
-    });
-};
+        });
+    };
 
-HttpUtil.deleteData = function(url, params, callback) {
-    $.ajax({
-        url: url,
-        type: 'DELETE',
-        contentType: 'application/json;charset=UTF-8',
-        dataType: 'json',
-        data: JSON.stringify(params),
-        error: function errorHandler(jqXHR, textStatus, errorThrown) {
-            callback("Error", null);
-        },
-        success: function successHandler(data, status, xhr) {
-            if (data.resultCode === 0) {
-                callback(null, data.data)
-            } else {
+    HttpUtil.putData = function(url, params, callback) {
+        $.ajax({
+            url: url,
+            type: 'PUT',
+            contentType: 'application/json;charset=UTF-8',
+            dataType: 'json',
+            data: JSON.stringify(params),
+            error: function errorHandler(jqXHR, textStatus, errorThrown) {
                 callback("Error", null);
+            },
+            success: function successHandler(data, status, xhr) {
+                if (data.resultCode === 0) {
+                    callback(null, data.data)
+                } else {
+                    callback("Error", null);
+                }
             }
-        }
-    });
-};
+        });
+    };
+
+    HttpUtil.deleteData = function(url, params, callback) {
+        $.ajax({
+            url: url,
+            type: 'DELETE',
+            contentType: 'application/json;charset=UTF-8',
+            dataType: 'json',
+            data: JSON.stringify(params),
+            error: function errorHandler(jqXHR, textStatus, errorThrown) {
+                callback("Error", null);
+            },
+            success: function successHandler(data, status, xhr) {
+                if (data.resultCode === 0) {
+                    callback(null, data.data)
+                } else {
+                    callback("Error", null);
+                }
+            }
+        });
+    };
+
+})(HttpUtil);
